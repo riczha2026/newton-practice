3600d3b7460ac2b11897a7bb523438ddbd2a85e8
riczha2026 <riczha200@gmail.com>
2026-08-19 12:05:58 -0700

    Checking and fixing formatting

---
+++ newton-save.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def first_derv(f,x,h=1e-5):
+    return (f(x+h)-f(x-h))/(2*h)
+    
+def sec_derv(f,x,h=1e-5):
+    return (f(x+h)-2*f(x)+f(x-h))/h**2
+    
+def optimize(start,f):
+    x = start
+    for i in range(1000):
+        f1=first_derv(f,x)
+        f2=sec_derv(f,x)
+        x_new = x-(f1/f2)
+        if abs(x_new-x)<1e-6:
+            break
+        x=x_new
+    return x
