3fe9ac193e55b6053fab37dbe2a598d5185c4593
riczha2026 <riczha200@gmail.com>
2026-08-19 12:12:23 -0700

    Adding Docstrings

--- newton.py
+++ newton.py
@@ -2,14 +2,17 @@ import numpy as np
 
 
 def first_derv(f, x, h=1e-5):
+    """Applying First Derivative"""
     return (f(x + h) - f(x - h)) / (2 * h)
 
 
 def sec_derv(f, x, h=1e-5):
+    """Applying Second Derivative"""
     return (f(x + h) - 2 * f(x) + f(x - h)) / h**2
 
 
 def optimize(start, f):
+    """Implementation of Newton's Method Formula"""
     x = start
     for i in range(1000):
         f1 = first_derv(f, x)
@@ -19,3 +22,6 @@ def optimize(start, f):
             break
         x = x_new
     return x
+
+
+help(optimize)
